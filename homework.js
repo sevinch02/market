aa625d77f958f02ce007a9f21d7b14adbee68828
Sevinch-najmiddinova <najmiddinova2021@mail.ru>
2021-09-07 21:47:13 +0500

    save last commit

---
+++ homework.js
@@ -0,0 +1,43 @@
+const div1 = document.getElementsByTagName('div')[0];
+const categories1 = document.getElementById('categories');
+const ulEl = document.getElementsByTagName('ul')
+const inputEl = document.getElementById('product');
+
+
+let a = function (){
+    let narx = Math.floor(Math.random() * 100) + '$';
+    return narx 
+ } 
+function categ(){
+     const divEl = document.createElement('div')
+     const imgEl = document.createElement('img');
+     const textEl = document.createElement('p');
+     const spanEl = document.createElement('span')
+     console.log(inputEl.value)
+     divEl.className = 'card'
+     textEl.innerText = inputEl.value;
+     inputEl.value = '';
+     spanEl.innerText = a();
+     imgEl.src = "https://i.pravatar.cc/300"
+     div1.appendChild(divEl);
+     divEl.appendChild(imgEl);
+     divEl.appendChild(textEl);
+     divEl.appendChild(spanEl)
+}
+function poisk(){
+    if(ulEl[0].style.display == 'block'){
+        ulEl[0].style.display = 'none' 
+    }
+    else {
+        ulEl[0].style.display = 'block'
+    }
+}
+
+// console.log(a() + '$')
+
+// a() == 'netto'
+
+
+
+
+
